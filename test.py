6b25ce26762071e4ccd8ba29f26fe72b5c2d9e49
raj13092002 <rajaryan13092002@gmail.com>
2024-03-18 13:10:24 +0530

    third commit

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ Data=[
     {"name":"ayush","age":55,"gender":"male","city":"pune"},
     {"name":"saurav","age":46,"gender":"male","city":"Kolkata"}
 ]
+
 Data=pd.DataFrame(Data)
 Data.to_csv("data/data.csv",index=False)
